2b42793aa9074d6beaae6053ffc502c2e870dd8f
Ruhama Yared <114031503+ruhamayared@users.noreply.github.com>
2023-05-19 21:25:30 -0400

    Time: 62 ms, Memory: 42.5 MB - LeetHub

---
+++ middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head
+    let fast = head
+    let secondMiddle = null
+    
+    while (fast && fast.next) {
+        slow = slow.next
+        secondMiddle = slow
+        fast = fast.next.next
+    }
+    
+    if (!secondMiddle) {
+        return slow
+    }
+    
+    return secondMiddle
+    
+};
